7e94495c1fad93b0ee95d10104d58c798b2e6faa
aididroslan <aididrsln@gmail.com>
2018-03-28 15:03:22 +0800

    Initial commit

---
+++ ShoppingBukuOnline/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingBukuOnline.Models
+{
+    public class Customer
+    {
+        public string id { set; get; }
+        public string name { set; get; }
+        public string phone { set; get; }
+        public string address { set; get; }
+        public string city { set; get; }
+        public string state { set; get; }
+        public string postcode { set; get; }
+    }
+}
---
+++ ShoppingBukuOnline/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingBukuOnline.Models
+{
+    public class User
+    {
+        public string email { set; get; }
+        public string password { set; get; }
+        public string userType { set; get; }
+    }
+}
